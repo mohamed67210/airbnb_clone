45d775ec4395ce183ecdd73758d47dff6cfb1503
mohamed67210 <mohamed-bechri@outlook.fr>
2023-08-26 15:37:37 +0200

    creation of responsive header  , install fontawesome package for icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,36 @@
+import Image from 'next/image'
 import React from 'react'
+import {faSearch,faGlobe,faBars,faUser} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   return (
-    <div>it's a header</div>
+    <header className='stickey top-0 z-50 grid grid-cols-3 h-20 bg-white shadow-md p-5 md:px-10'>
+        {/* left  */}
+        <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+            <Image src="https://links.papareact.com/qd3" 
+            alt='image' 
+            layout='fill'
+            objectFit='contain'
+            objectPosition='left'
+            />
+        </div>
+        {/* middle  */}
+        <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+            <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-500' type="text" placeholder='Start your search' />
+            {/* <button className=' bg-red-800 text-white p-2 cursor-pointer'>Search</button> */}
+            <FontAwesomeIcon icon={faSearch} className='hidden md:inline-flex h-8 w-8 p-2 rounded-full text-white bg-red-700 cursor-pointer md:mx-2'/>
+        </div>
+        {/* right  */}
+        <div className='flex items-center space-x-4 justify-end'>
+            <p className='hidden md:inline'>become a host</p>
+            <FontAwesomeIcon icon={faGlobe} className=' md:inline-flex h-8 w-8 p-2 rounded-full text-white bg-red-700 cursor-pointer md:mx-2'/>
+            <div className='flex items-center p-2 border-2 rounded-xl'>
+                <FontAwesomeIcon icon={faBars} className=' md:inline-flex h-8 w-8 p-2 rounded-full text-white bg-red-700 cursor-pointer md:mx-2'/>
+                <FontAwesomeIcon icon={faUser} className=' md:inline-flex h-8 w-8 p-2 rounded-full text-white bg-red-700 cursor-pointer md:mx-2'/>
+            </div>
+        </div>
+    </header>
   )
 }
 
