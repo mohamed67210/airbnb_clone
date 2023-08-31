00970032c707bb4c2e5577ae9ab2d0123d385a6c
mohamed67210 <mohamed-bechri@outlook.fr>
2023-08-31 12:37:40 +0200

    update header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
+'use client' // pour dire a node js que c un composent client pour l'utilisation de usestate
 import Image from 'next/image'
-import React from 'react'
 import {faSearch,faGlobe,faBars,faUser} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { useState } from 'react';
+
+
 function Header() {
+    const [searchInput,setSearchInput] = useState('');
   return (
-    <header className='stickey top-0 z-50 grid grid-cols-3 gap-3 h-20 bg-white p-5 md:px-10'>
+    <header className='sticky top-0 z-50 grid grid-cols-3 gap-3 p-5 md:px-10 '>
         {/* left  */}
         <div className='relative flex items-center h-10 cursor-pointer my-auto'>
             <Image src="https://links.papareact.com/qd3" 
@@ -16,14 +20,18 @@ function Header() {
             />
         </div>
         {/* middle  */}
-        <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-            <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-500' type="text" placeholder='Start your search' />
+        <div className='relative flex items-center md:border-2 rounded-full py-2 md:shadow-sm h-12  my-auto'>
+            <input
+            value={searchInput}
+            className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-500' 
+            type="text" 
+            placeholder='Start your search' />
             {/* <button className=' bg-red-800 text-white p-2 cursor-pointer'>Search</button> */}
-            <FontAwesomeIcon icon={faSearch} className='hidden md:inline-flex h-8 w-8 p-2 rounded-full text-white bg-red-700 cursor-pointer md:mx-2'/>
+            <FontAwesomeIcon icon={faSearch} className='absolute right-0 md:inline-flex h-8 w-8 p-2 rounded-full text-white bg-red-700 cursor-pointer md:mx-2'/>
         </div>
         {/* right  */}
-        <div className='flex items-center space-x-4 justify-end'>
-            <button className='hidden md:inline hover:bg-gray-100 p-4 rounded-full text-sm'>Mettre mon logement sur Airbnb</button>
+        <div className='flex items-center space-x-4 h-16 justify-end'>
+            <button className='hidden md:inline hover:bg-gray-100 p-4 rounded-full text-sm sm:text-xs'>Mettre mon logement sur Airbnb</button>
             <FontAwesomeIcon icon={faGlobe} className=' md:inline-flex h-8 w-8 p-2 rounded-full text-white bg-red-700 cursor-pointer md:mx-2'/>
             <div className='flex items-center p-1 border-2 rounded-xl hover:shadow-lg cursor-pointer'>
                 <FontAwesomeIcon icon={faBars} className=' md:inline-flex h-8 w-8 p-2 cursor-pointer md:mx-2'/>
