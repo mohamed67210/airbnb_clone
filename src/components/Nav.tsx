2f9dd1dc39841bb0fd242163388f8e46d7248a33
mohamed67210 <mohamed-bechri@outlook.fr>
2023-09-07 10:44:40 +0200

    edit style icons nav pour responsive

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,23 +6,23 @@ function Nav() {
   return (
     <div className='sticky top-20 flex items-center justify-center gap-8 p-8 shadow-md bg-white z-40'>
         <article className='flex flex-col items-center justify-center gap-3 text-gray-500 cursor-pointer'>
-            <FontAwesomeIcon icon={faFire} className='h-8 w-8' />
+            <FontAwesomeIcon icon={faFire} className='h-5 w-5 md:h-8 md:w-8' />
             <p>Tendance</p>
         </article>
         <article className='flex flex-col items-center justify-center gap-3 text-gray-500 cursor-pointer'>
-            <FontAwesomeIcon icon={faWaterLadder} className='h-8 w-8' />
+            <FontAwesomeIcon icon={faWaterLadder} className='h-5 w-5 md:h-8 md:w-8' />
             <p>Piscine</p>
         </article>
         <article className='flex flex-col items-center justify-center gap-3 text-gray-500 cursor-pointer'>
-            <FontAwesomeIcon icon={faFontAwesome} className='h-8 w-8' />
+            <FontAwesomeIcon icon={faFontAwesome} className='h-5 w-5 md:h-8 md:w-8' />
             <p>Chateau</p>
         </article>
         <article className='flex flex-col items-center justify-center gap-3 text-gray-500 cursor-pointer'>
-            <FontAwesomeIcon icon={faWater} className='h-8 w-8' />
+            <FontAwesomeIcon icon={faWater} className='h-5 w-5 md:h-8 md:w-8' />
             <p>au bord de la mer</p>
         </article>
         <article className='flex flex-col items-center justify-center gap-3 text-gray-500 cursor-pointer'>
-            <FontAwesomeIcon icon={faMugHot} className='h-8 w-8' />
+            <FontAwesomeIcon icon={faMugHot} className='h-5 w-5 md:h-8 md:w-8' />
             <p>Chambre d'hote</p>
         </article>
     </div>
